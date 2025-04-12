58d59c3b23b1e598eac0577d4fcb456c7bfae1c3
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-04-13 00:10:19 +0900

    Removed write()

--- example/server.zig
+++ example/server.zig
@@ -34,17 +34,6 @@ pub fn main() !void {
     }
 }
 
-fn write(socket: posix.socket_t, msg: []const u8) !void {
-    var pos: usize = 0;
-    while (pos < msg.len) {
-        const written = try posix.write(socket, msg[pos..]);
-        if (written == 0) {
-            return error.Closed;
-        }
-        pos += written;
-    }
-}
-
 const Client = struct {
     socket: posix.socket_t,
     address: std.net.Address,
