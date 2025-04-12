c3168b3c0af1f931c910607137f69075868d95ee
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-04-12 20:10:30 +0900

    Convert project to a proper library with public API exports

--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,6 @@
 // https://www.jsonrpc.org/specification
 
 // Tests will be written in here
+pub const RequestObject = @import("type.zig").RequestObject;
+pub const ResponseObject = @import("type.zig").ResponseObject;
+pub const JsonRpcVersion = @import("type.zig").JsonRpcVersion;
